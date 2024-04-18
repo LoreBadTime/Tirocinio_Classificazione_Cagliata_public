ab3b5886a8276a12312f68402f55541be79f9e75
Lorenzo <concas324@gmail.com>
2024-04-18 23:37:53 +0200

    updated tests description

--- test_extractors.bat
+++ test_extractors.bat
@@ -2,8 +2,9 @@
 :: automatic test for set 5 (see create_test_set.pt)
 
 :: creating merge set (N_sets-set number 5)
-::python -m create_test_set
+python -m create_test_set
 
+:: mutual information test(COMMENT THIS TEST IF SCIKIT/NUMPY ARE NOT INSTALLED)
 echo on
 set var=%~dp0test\test_feature_selection
 set var=%var:\=/%
@@ -14,9 +15,18 @@ set var=%var:\=/%
 :: python outputs just a list of scores normally  
 matlab -batch "run('%var%.m')" && python test/test1.py
 
+:: every test made (Enhanced, set 5), PARSED REPORTING AVAIABLE ONLY USING PYTHON 
+:: k_fold sequential inset 
 start /high runmatlab.bat kfold_extractionFromModel 5 Enhanced 1
+:: k_fold shuffle inset
 start /high runmatlab.bat shuffle_kfold_extractionFromModel 5 Enhanced 1
+:: n-1 marge,n excluded in test sequential
 start /high runmatlab.bat set_extractionFromModel 5 Enhanced 1
+:: n-1 marge,n excluded in test shuffle
 start /high runmatlab.bat set_shuffle_extractionFromModel 5 Enhanced 1
+:: n-1 merge,n exluded feature selection Mutual info static (already-calculated features to select in a file)
 start /high runmatlab.bat set_fselectionmerged_shuffle_extractionFromModel 5 Enhanced 1
+:: n-1 merge,n exluded feature selection Mutual info dynamic (features to select calculated on the go)
 start /high runmatlab.bat set_fselectionmerged_shuffle_extractionFromModel 5 Enhanced 2
+
+:: enseble tests not provided for now (needs special configuration)
